fed1733170142064dbfd0d9604411ae55f9574c2
Tapas66 <83996965+Tapas66@users.noreply.github.com>
2026-04-06 16:20:47 +0530

    Refactor teamMembers array with new IDs
    Updated team member IDs and removed some entries.

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -8,48 +8,24 @@ export interface TeamMember {
 export const teamMembers: TeamMember[] = [
   {
     id: '1',
-    name: 'Tapas Pandey',
-    role: 'Student',
-    image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
-  },
-  {
-    id: '2',
-    name: 'Pranav Sahu',
-    role: 'Student',
-    image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
-  },
-  {
-    id: '3',
     name: 'Vedant Jha',
     role: 'Student',
     image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
   },
   {
-    id: '4',
+    id: '2',
     name: 'Swadha Shubhangi',
     role: 'Student',
     image: 'https://cdn-icons-png.flaticon.com/512/1999/1999629.png' // cartoon girl
   },
   {
-    id: '5',
-    name: 'Ayush Kumar',
-    role: 'Student',
-    image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
-  },
-  {
-    id: '6',
+    id: '3',
     name: 'Mohank Saini',
     role: 'Student',
     image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
   },
   {
-    id: '7',
-    name: 'Ambuj Rai',
-    role: 'Student',
-    image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
-  },
-  {
-    id: '8',
+    id: '4',
     name: 'Priyanshu Prasad',
     role: 'Student',
     image: 'https://cdn-icons-png.flaticon.com/512/1999/1999625.png' // cartoon boy
